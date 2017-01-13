2129fedbcce5c2c9624185ce66f9cfbb0e287771
cristinal <cristina@klab.com>
2017-01-13 17:06:59 +0800

    organized files

--- app/src/main/java/com/mtinalo/practice/anilist/activities/MainActivity.java
+++ app/src/main/java/com/mtinalo/practice/anilist/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mtinalo.practice.anilist;
+package com.mtinalo.practice.anilist.activities;
 
 /**
  * Created by ArthurDKid on 1/11/2017.
--- app/src/main/java/com/mtinalo/practice/anilist/fragments/MangaGalleryFragment.java
+++ app/src/main/java/com/mtinalo/practice/anilist/fragments/MangaGalleryFragment.java
@@ -1,4 +1,4 @@
-package com.mtinalo.practice.anilist;
+package com.mtinalo.practice.anilist.fragments;
 
 /**
  * Created by ArthurDKid on 1/11/2017.
